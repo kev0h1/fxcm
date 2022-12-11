c90fa0da863abf5d1e514a65b44979258669a3ad
kev0h1 <45684757+kev0h1@users.noreply.github.com>
2022-12-11 10:11:57 +0000

    added git ignore

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
-
 from src.config import ForexPairEnum, PeriodEnum
 from src.fxcm_connect.fxcm_connect import FXCMConnect, config
+from bs4 import BeautifulSoup
+import requests
+import html5lib
 
 
 if __name__ == "__main__":
-    hello = FXCMConnect(conf=config)
-    data = hello.get_candle_data(ForexPairEnum.USDCAD, PeriodEnum.DAY)
-    data = hello.get_refined_data(data)
-    print("hiii")
+    url = "https://relatedwords.io/weight-loss"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html5lib")
+    arr = soup.find_all("a")
+
+    f = open("my_data2.txt", "a")
+    f.write("data\n")
+    for item in arr:
+        try:
+            f.write(item.text + "\n")
+        except:
+            pass
+
+    f.close()
