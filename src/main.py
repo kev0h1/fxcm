b9b99f084f0745aa575cbb70736c41421d3e0f31
kev0h1 <45684757+kev0h1@users.noreply.github.com>
2022-12-16 19:00:41 +0000

    missing files

--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
+import html5lib
+import requests
+from bs4 import BeautifulSoup
+
 from src.config import ForexPairEnum, PeriodEnum
 from src.fxcm_connect.fxcm_connect import FXCMConnect, config
-from bs4 import BeautifulSoup
-import requests
-import html5lib
 
 # dummmy
 
 if __name__ == "__main__":
-    url = "https://relatedwords.io/weight-loss"
+    url = "https://relatedwords.io/social-media"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html5lib")
     arr = soup.find_all("a")
 
-    f = open("my_data2.txt", "a")
+    f = open("social.txt", "a")
     f.write("data\n")
     for item in arr:
         try:
