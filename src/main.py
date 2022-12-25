1b7b34a9074d20f0dcbbc3598a0dbc01a835b3da
Kevin <kevin.maingi12@gmail.com>
2022-12-25 22:19:32 +0300

    clean up main

--- src/main.py
+++ src/main.py
@@ -1,24 +1,2 @@
-import html5lib
-import requests
-from bs4 import BeautifulSoup
-from src.config import ForexPairEnum, PeriodEnum
-from src.fxcm_connect.fxcm_connect import FXCMConnect, config
-
-# dummmy
-
 if __name__ == "__main__":
-    url = "https://relatedwords.io/job"
-    page = requests.get(url, verify=False)
-    soup = BeautifulSoup(page.content, "html5lib")
-    arr = soup.find_all("a")
-
-    f = open("job.txt", "a")
-    f.write("data\n")
-
-    for item in arr:
-        try:
-            f.write(item.text + "\n")
-        except:
-            pass
-
-    f.close()
+    pass
