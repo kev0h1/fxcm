6ec070c93c14dde72b89f901548b01947ca82b15
Kevin <kevin.maingi12@gmail.com>
2023-07-22 19:15:43 +0100

    add schedules

--- src/entry_points/scheduler/scheduler.py
+++ src/entry_points/scheduler/scheduler.py
@@ -5,6 +5,11 @@ from src.entry_points.scheduler.get_technical_signal import (
     get_technical_signal,
 )
 
+from src.entry_points.scheduler.manage_trades import manage_trades
+from src.entry_points.scheduler.manage_closed_trades import (
+    manage_closed_trades,
+)
+
 from src.logger import get_logger
 
 logger = get_logger(__name__)
@@ -14,7 +19,7 @@ scheduler = AsyncIOScheduler()
 
 @scheduler.scheduled_job("interval", seconds=300)
 async def get_fundamental_trend_data():
-    date_: datetime = datetime(2023, 7, 8)
+    date_: datetime = datetime.today()
     if date_.weekday() < 5:
         logger.info(f"Getting fundamental data for {date_}")
         await process_data(date_=date_)
@@ -26,3 +31,19 @@ async def get_fundamental_technical_data():
     if date_.weekday() < 5:
         logger.info(f"Getting fundamental data for {date_}")
         await get_technical_signal()
+
+
+@scheduler.scheduled_job("interval", seconds=60)
+async def manage_trades():
+    date_: datetime = datetime.today()
+    if date_.weekday() < 5:
+        logger.info(f"Manage trades for date{date_}")
+        await manage_trades()
+
+
+@scheduler.scheduled_job("interval", seconds=300)
+async def manage_closed_trades():
+    date_: datetime = datetime.today()
+    if date_.weekday() < 5:
+        logger.info(f"Manage closed trades for date {date_}")
+        await manage_closed_trades()
