2d4850c3b8bf48b885e78098b1189656c2f44f56
Kevin <kevin.maingi12@gmail.com>
2024-02-21 22:38:13 +0000

    make concurrent

--- src/entry_points/scheduler/get_technical_signal.py
+++ src/entry_points/scheduler/get_technical_signal.py
@@ -13,6 +13,7 @@ from src.domain.events import (
 )
 from src.logger import get_logger
 import pandas as pd
+import asyncio
 
 logger = get_logger(__name__)
 
@@ -24,82 +25,85 @@ async def get_technical_signal(
 ) -> None:  # type: ignore
     """Gets the technical signal for the currency"""
     async with uow:
+        tasks = []
         for forex_pair in ForexPairEnum.__members__.values():
-            refined_data: pd.DataFrame = await uow.fxcm_connection.get_candle_data(
-                instrument=ForexPairEnum(forex_pair),
-                period=PeriodEnum.MINUTE_15,
-                number=250,
+            tasks.append(generate_signal_for_forex_pair(uow, indicator, forex_pair))
+        await asyncio.gather(*tasks)
+
+
+async def generate_signal_for_forex_pair(uow, indicator, forex_pair):
+    logger.info("Getting data for forex pair %s" % forex_pair)
+    refined_data: pd.DataFrame = await uow.fxcm_connection.get_candle_data(
+        instrument=ForexPairEnum(forex_pair),
+        period=PeriodEnum.MINUTE_15,
+        number=250,
+    )
+
+    # Choose strategy here
+    refined_data = await divergence_convergence(refined_data, indicator)
+
+    # Combine the Buy_Signal and Sell_Signal into a single Signal column
+    refined_data["Signal"] = refined_data["Buy_Signal"].replace(
+        {True: 1, False: 0}
+    ) - refined_data["Sell_Signal"].replace({True: 1, False: 0})
+
+    refined_data = refined_data.drop(columns=["Buy_Signal", "Sell_Signal"])
+
+    def calculate_stop(row):
+        atr_multiplier = 2
+        if row["Signal"] == 1:  # Buy
+            return (
+                row["close"] - atr_multiplier * row["atr"]
+            )  # Adjust the multiplier as needed
+        elif row["Signal"] == -1:  # Sell
+            return (
+                row["close"] + atr_multiplier * row["atr"]
+            )  # Adjust the multiplier as needed
+        else:  # No signal
+            return None
+
+    def calculate_limit(row):
+        atr_multiplier = 8
+        # if row["Signal"] == 1:  # Buy
+        #     return (
+        #         row["close"] + atr_multiplier * row["atr"]
+        #     )  # Adjust the multiplier as needed
+        # elif row["Signal"] == -1:  # Sell
+        #     return (
+        #         row["close"] - atr_multiplier * row["atr"]
+        #     )  # Adjust the multiplier as needed
+        # else:  # No signal
+        return None
+
+    refined_data["ATR_Stop"] = refined_data.apply(calculate_stop, axis=1)
+    refined_data["ATR_Limit"] = refined_data.apply(calculate_limit, axis=1)
+
+    if refined_data.iloc[-1]["Signal"] > 0:
+        logger.warning("Bullish Signal generated for %s" % forex_pair)
+        await uow.publish(
+            CloseForexPairEvent(forex_pair=forex_pair, sentiment=SentimentEnum.BEARISH)
+        )
+        await uow.publish(
+            OpenTradeEvent(
+                forex_pair=forex_pair,
+                sentiment=SentimentEnum.BULLISH,
+                stop=refined_data.iloc[-1]["ATR_Stop"],
+                close=refined_data.iloc[-1]["close"],
             )
-
-            # Choose strategy here
-            refined_data = await divergence_convergence(refined_data, indicator)
-
-            # Combine the Buy_Signal and Sell_Signal into a single Signal column
-            refined_data["Signal"] = refined_data["Buy_Signal"].replace(
-                {True: 1, False: 0}
-            ) - refined_data["Sell_Signal"].replace({True: 1, False: 0})
-
-            refined_data = refined_data.drop(columns=["Buy_Signal", "Sell_Signal"])
-
-            def calculate_stop(row):
-                atr_multiplier = 2
-                if row["Signal"] == 1:  # Buy
-                    return (
-                        row["close"] - atr_multiplier * row["atr"]
-                    )  # Adjust the multiplier as needed
-                elif row["Signal"] == -1:  # Sell
-                    return (
-                        row["close"] + atr_multiplier * row["atr"]
-                    )  # Adjust the multiplier as needed
-                else:  # No signal
-                    return None
-
-            def calculate_limit(row):
-                atr_multiplier = 8
-                # if row["Signal"] == 1:  # Buy
-                #     return (
-                #         row["close"] + atr_multiplier * row["atr"]
-                #     )  # Adjust the multiplier as needed
-                # elif row["Signal"] == -1:  # Sell
-                #     return (
-                #         row["close"] - atr_multiplier * row["atr"]
-                #     )  # Adjust the multiplier as needed
-                # else:  # No signal
-                return None
-
-            refined_data["ATR_Stop"] = refined_data.apply(calculate_stop, axis=1)
-            refined_data["ATR_Limit"] = refined_data.apply(calculate_limit, axis=1)
-
-            if refined_data.iloc[-1]["Signal"] > 0:
-                logger.warning("Bullish Signal generated for %s" % forex_pair)
-                await uow.publish(
-                    CloseForexPairEvent(
-                        forex_pair=forex_pair, sentiment=SentimentEnum.BEARISH
-                    )
-                )
-                await uow.publish(
-                    OpenTradeEvent(
-                        forex_pair=forex_pair,
-                        sentiment=SentimentEnum.BULLISH,
-                        stop=refined_data.iloc[-1]["ATR_Stop"],
-                        close=refined_data.iloc[-1]["close"],
-                    )
-                )
-            elif refined_data.iloc[-1]["Signal"] < 0:
-                logger.warning("Bearish Signal generated for %s" % forex_pair)
-                await uow.publish(
-                    CloseForexPairEvent(
-                        forex_pair=forex_pair, sentiment=SentimentEnum.BULLISH
-                    )
-                )
-                await uow.publish(
-                    OpenTradeEvent(
-                        forex_pair=forex_pair,
-                        sentiment=SentimentEnum.BEARISH,
-                        stop=refined_data.iloc[-1]["ATR_Stop"],
-                        close=refined_data.iloc[-1]["close"],
-                    )
-                )
+        )
+    elif refined_data.iloc[-1]["Signal"] < 0:
+        logger.warning("Bearish Signal generated for %s" % forex_pair)
+        await uow.publish(
+            CloseForexPairEvent(forex_pair=forex_pair, sentiment=SentimentEnum.BULLISH)
+        )
+        await uow.publish(
+            OpenTradeEvent(
+                forex_pair=forex_pair,
+                sentiment=SentimentEnum.BEARISH,
+                stop=refined_data.iloc[-1]["ATR_Stop"],
+                close=refined_data.iloc[-1]["close"],
+            )
+        )
 
 
 async def get_signal(refined_data: pd.DataFrame) -> pd.DataFrame:
--- src/entry_points/scheduler/manage_trades.py
+++ src/entry_points/scheduler/manage_trades.py
@@ -10,6 +10,7 @@ from src.service_layer.uow import MongoUnitOfWork
 from src.logger import get_logger
 from src.entry_points.scheduler.manage_closed_trades import update_trade_state
 from src.utils import close_trade_in_oanda_util
+import asyncio
 
 logger = get_logger(__name__)
 
@@ -23,72 +24,71 @@ async def manage_trades_handler(
         forex_pairs: list[ForexPairEnum] = (
             await uow.trade_repository.get_distinct_forex_pairs()
         )
+        tasks = []
         for forex_pair in forex_pairs:
-            data = await uow.fxcm_connection.get_candle_data(
-                forex_pair, PeriodEnum.MINUTE_5, 20
-            )
-            data = await indicator.get_atr(data, 14)
-            close = data.iloc[-1]["close"]
-            atr = data.iloc[-1]["atr"]
+            tasks.append(manage_trade_for_currency_pair(uow, indicator, forex_pair))
 
-            trades: list[Trade] = (
-                await uow.trade_repository.get_open_trades_by_forex_pair(
-                    forex_pair=forex_pair
+        await asyncio.gather(*tasks)
+
+
+async def manage_trade_for_currency_pair(uow, indicator, forex_pair):
+    data = await uow.fxcm_connection.get_candle_data(
+        forex_pair, PeriodEnum.MINUTE_5, 20
+    )
+    data = await indicator.get_atr(data, 14)
+    close = data.iloc[-1]["close"]
+    atr = data.iloc[-1]["atr"]
+
+    trades: list[Trade] = await uow.trade_repository.get_open_trades_by_forex_pair(
+        forex_pair=forex_pair
+    )
+    multiplier = 2
+
+    pip_value = 0.0001 if "JPY" not in forex_pair.value.split("/") else 0.01
+
+    gbp_per_pip = await calculate_gbp_pips(forex_pair, uow, pip_value)
+
+    for trade in trades:
+        modified = False
+
+        half_spread_pips = (trade.half_spread_cost / gbp_per_pip) * pip_value * 1.25
+
+        if trade.is_buy:
+            adjusted_close_long = close
+            new_stop = adjusted_close_long - multiplier * atr
+            if new_stop > trade.stop and new_stop > (trade.close + half_spread_pips):
+                # get the pip location =
+                trade.stop = new_stop
+                modified = True
+        else:
+            adjusted_close_short = close
+            new_stop = adjusted_close_short + multiplier * atr
+            if new_stop < trade.stop and new_stop < (trade.close - half_spread_pips):
+                trade.stop = new_stop
+                modified = True
+
+        if modified:
+            logger.warning(
+                "Modified trade %s with signal of %s to have a new stop of %s"
+                % (
+                    trade.trade_id,
+                    trade.is_buy,
+                    trade.stop,
                 )
             )
-            multiplier = 2
+            await uow.trade_repository.save(trade)
 
-            pip_value = 0.0001 if "JPY" not in forex_pair.value.split("/") else 0.01
-
-            gbp_per_pip = await calculate_gbp_pips(forex_pair, uow, pip_value)
-
-            for trade in trades:
-                modified = False
-
-                half_spread_pips = (
-                    (trade.half_spread_cost / gbp_per_pip) * pip_value * 1.25
-                )
-
-                if trade.is_buy:
-                    adjusted_close_long = close
-                    new_stop = adjusted_close_long - multiplier * atr
-                    if new_stop > trade.stop and new_stop > (
-                        trade.close + half_spread_pips
-                    ):
-                        # get the pip location =
-                        trade.stop = new_stop
-                        modified = True
-                else:
-                    adjusted_close_short = close
-                    new_stop = adjusted_close_short + multiplier * atr
-                    if new_stop < trade.stop and new_stop < (
-                        trade.close - half_spread_pips
-                    ):
-                        trade.stop = new_stop
-                        modified = True
-
-                if modified:
-                    logger.warning(
-                        "Modified trade %s with signal of %s to have a new stop of %s"
-                        % (
-                            trade.trade_id,
-                            trade.is_buy,
-                            trade.stop,
-                        )
-                    )
-                    await uow.trade_repository.save(trade)
-
-                if (trade.is_buy and trade.stop > (close + half_spread_pips)) or (
-                    not trade.is_buy and trade.stop < (close - half_spread_pips)
-                ):
-                    logger.info(
-                        "Trade crossed stop loss for trade %s, with stop of %s and is buy is %s"
-                        % (trade.trade_id, trade.stop, trade.is_buy),
-                    )
-                    trade.position = PositionEnum.CLOSED
-                    await close_trade_in_oanda_util(uow, trade)
-                    # there reason this didnt work is because you didnt save
-                    await uow.trade_repository.save(trade)
+        if (trade.is_buy and trade.stop > (close + half_spread_pips)) or (
+            not trade.is_buy and trade.stop < (close - half_spread_pips)
+        ):
+            logger.info(
+                "Trade crossed stop loss for trade %s, with stop of %s and is buy is %s"
+                % (trade.trade_id, trade.stop, trade.is_buy),
+            )
+            trade.position = PositionEnum.CLOSED
+            await close_trade_in_oanda_util(uow, trade)
+            # there reason this didnt work is because you didnt save
+            await uow.trade_repository.save(trade)
 
 
 async def calculate_gbp_pips(
